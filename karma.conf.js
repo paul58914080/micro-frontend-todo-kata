ff40d723e05e6d45f6844c205a4f763aa6577368
paul58914080 <paul58914080@gmail.com>
2019-07-22 10:01:25 +0530

    feat(angular): upgraded to version 8

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/micro-frontend-todo'),
+      dir: require('path').join(__dirname, './coverage/micro-frontend-todo'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
